1be28f2614c0682eb38e2a8aea0757abd7e0ba8b
petarpop88 <petarpop88@gmail.com>
2022-09-21 19:59:05 +0200

    KlasaZaPromeneMnozenje

---
+++ src/KlasaZaPromene.java
@@ -0,0 +1,12 @@
+public class KlasaZaPromene {
+
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 2;
+        int z = x * y;
+
+        System.out.println(z);
+    }
+}
